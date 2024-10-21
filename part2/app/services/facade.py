35ea54c2ae561389335500bf43f68500da9a3555
Wqkz <guillaume.farina@hotmail.com>
2024-10-21 06:20:03 +0200

    Add function for Amenity Class

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -2,7 +2,7 @@
 
 from app.persistence.repository import InMemoryRepository
 from ..models.user import User
-
+from ..models.amenity import Amenity
 
 class HBnBFacade:
     def __init__(self):
@@ -11,6 +11,9 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
+    """ 
+    USER
+    """
     def create_user(self, user_data):
         # Placeholder method for creating a user
         user = User(**user_data)
@@ -38,3 +41,30 @@ class HBnBFacade:
         if obj:
             obj.update(user_data)
         return obj
+
+    """ 
+    AMENITY 
+    """
+    def create_amenity(self, amenity_data):
+        # Placeholder for logic to create an amenity
+        amenity = Amenity(**amenity_data)
+        Amenity.validate_request_data(amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        # Placeholder for logic to retrieve an amenity by ID
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        # Placeholder for logic to retrieve all amenities
+        return list(self.amenity_repo.get_all())
+
+    def update_amenity(self, amenity_id, amenity_data):
+        # Placeholder for logic to update an amenity
+        Amenity.validate_request_data(amenity_data)
+        obj = self.get_amenity(amenity_id)
+        if obj:
+            obj.update(amenity_data)
+        return obj
+       
