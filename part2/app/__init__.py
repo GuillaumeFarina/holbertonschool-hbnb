f6b5a2a886af4f647a241d2e7d47ef12c428436b
Wqkz <guillaume.farina@hotmail.com>
2024-10-21 04:06:26 +0200

    add new line

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from flask_restx import Api
-
+from app.api.v1.users import api as users_ns
 
 def create_app():
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API',
-              description='API de l’application HBnB')
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
-    # Espace réservé pour les namespaces de l’API (les points de terminaison seront ajoutés plus tard)
-    # Des namespaces supplémentaires pour places, reviews, et amenities seront ajoutés plus tard
-
+    # Register the users namespace
+    api.add_namespace(users_ns, path='/api/v1/users')
     return app
