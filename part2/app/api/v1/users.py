2b1ab100041e4f28254b57d567fb8efbe806b858
Wqkz <guillaume.farina@hotmail.com>
2024-10-23 22:38:47 +0200

    Change my return for a dict user

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -23,6 +23,15 @@ def validate_email(email):
         raise ValueError("Invalid email format")
     return email
 
+class User:
+    def __init__(self, id, first_name, last_name, email, password, is_owner=False):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.is_owner = is_owner
+
 @api.route('/')
 class UserList(Resource):
     @api.expect(user_model, validate=True)
@@ -47,24 +56,25 @@ class UserList(Resource):
             new_user = facade.create_user(user_data)
         except Exception as e:
             return {"Error": str(e)}, 500
-        return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email, 'is_owner': new_user.is_owner}, 201
+        return new_user.to_dict(), 201
 
     @api.response(200, 'List of users retrieved successfully')
     def get(self):
         """Retrieve the list of users"""
         users = facade.get_all_users()
-        return [{'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email, 'is_owner': user.is_owner} for user in users], 200
+        return [user.to_dict() for user in users], 200
 
 @api.route('/<user_id>')
 class UserResource(Resource):
     @api.response(200, 'User details retrieved successfully')
     @api.response(404, 'User not found')
+    @api.response(400, 'Invalid input data')
     def get(self, user_id):
         """Get user details by ID"""
         user = facade.get_user(user_id)
         if not user:
             return {'error': 'User not found'}, 404
-        return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email, 'is_owner': user.is_owner}, 200
+        return user.to_dict(), 200
 
     @api.expect(user_model, validate=True)
     @api.response(200, 'User successfully updated')
@@ -83,4 +93,4 @@ class UserResource(Resource):
         updated_user = facade.update_user(user_id, user_data)
         if not updated_user:
             return {'error': 'User not found'}, 404
-        return {'id': updated_user.id, 'first_name': updated_user.first_name, 'last_name': updated_user.last_name, 'email': updated_user.email, 'is_owner': updated_user.is_owner}, 200
+        return updated_user.to_dict(), 200
